89c3775d641150f3a2ef0ff4f2cf3dabcc1786f6
KYapp <kukiyomenaisapp@gmail.com>
2023-02-19 01:16:12 +0900

    Revert "nazo"
    This reverts commit 8bbb5e645d5a72b9d3c28dc00c151e7c94ff0c56.

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using JuhaKurisu.PopoTools.ByteSerializer;
 using System.Linq;
+using System.Text;
 using NativeWebSocket;
 
 namespace JuhaKurisu.NgrokSpeedTest
@@ -23,15 +24,17 @@ namespace JuhaKurisu.NgrokSpeedTest
             webSocket.OnMessage += (e) =>
                 Debug.Log("message");
             webSocket.OnError += (e) =>
-                Debug.Log("error");
+                Debug.Log($"error {string.Join("", e)}");
             webSocket.OnClose += (bytes) =>
                 Debug.Log("close");
 
-            // Keep sending messages at every 0.3s
-            InvokeRepeating("SendWebSocketMessage", 0.0f, 0.3f);
-
             // waiting for messages
             await webSocket.Connect();
+        }
+
+        private async void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A)) await webSocket.SendText("hoge");
         }
 
         private void OnDestroy()
